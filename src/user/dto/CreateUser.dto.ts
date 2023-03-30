eb95fe631147bdc30e8fcb1e9232d1cc33eef848
Leonardo Dias Cavalcanti <leonardodiass@gmail.com>
2023-03-30 12:51:32 -0300

    added validations

--- src/user/dto/CreateUser.dto.ts
+++ src/user/dto/CreateUser.dto.ts
@@ -1,5 +1,12 @@
+import { IsString } from "class-validator";
+import { IsEmail, IsNotEmpty, IsStrongPassword } from "class-validator/types/decorator/decorators";
+
 export class CreateUserDTO{
+    @IsString()
+    @IsNotEmpty()
     name: string;
+    @IsEmail()
     email: string;
+    @IsStrongPassword()
     pswd: string;
 }
